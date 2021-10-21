13b00bf5907c763089c3a598b7f3a7c8a0645d65
Ryan Ewing <ewing.ryan@gmail.com>
2021-10-21 13:37:24 -0700

    updating employer using vscode.dev

--- src/pages/now.js
+++ src/pages/now.js
@@ -11,7 +11,7 @@ const SecondPage = () => (
     <p><strong>Think</strong>ing about how to manage the intake and triage of articles, books, videos, and other content that I want to consume without destroying the storage medium (todoist, notion, email, text file, pocket). I've failed everywhere.</p>
     <p><strong>Read</strong>ing <a href="https://samharris.org/books/lying/">Lying</a> by Sam Harris, as well as <a href="https://en.wikipedia.org/wiki/Dune_(novel)">Dune</a> by Frank Herbert.</p>
     <p><strong>Learn</strong>ing about 3D printing. My first printer comes this month!</p>
-    <p><strong>Work</strong>ing for SAP Concur as an Engineering Manager leading the Expense UI team.</p>
+    <p><strong>Work</strong>ing for Compass (Modus) as an Engineering Manager</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
